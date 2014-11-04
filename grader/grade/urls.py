1e2d0a4e3ae331fe2a24df0d79f1bce1d5d4ff7b
Riitta Hietaranta <namiskuukkel@gmail.com>
2014-11-04 04:22:29 -0500

    /grade/ url korjattu

--- grader/grade/urls.py
+++ grader/grade/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls import patterns, url
 import views
 
 urlpatterns = patterns('',
-    url(r'/grade/$', views.grade, name='grade'),
+    url(r'/grade', views.grade, name='grade'),
     url(r'$', views.code, name='code'),
 )
