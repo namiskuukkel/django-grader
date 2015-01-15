107eabfa86912a42d7522996700f9162b1792e3a
namiskuukkel <namiskuukkel@gmail.com>
2015-01-15 10:37:59 +0200

    Test separated

---
+++ grader/kurssi/Harjoitus1/test1.py
@@ -0,0 +1,7 @@
+type = "compare_output"
+name =  "Testi 1"
+description = "Tulostetaan stringi"
+timeout = 15
+test_results = ["Jokin meni pieleen", "expected"]
+test_result_limits = [5, 0]
+points = 2
